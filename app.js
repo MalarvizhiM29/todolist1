418e1e6bfe78d95ef4a66305bea91371365c9075
MALARVIZHI M <Malarm2004@gmail.com>
2023-06-06 14:09:44 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://127.0.0.1:27017/todolistDB', {
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
   useNewUrlParser: true,
-})
+});
 
 const itemsSchema = new mongoose.Schema({
   name: String,
@@ -21,135 +21,143 @@ const itemsSchema = new mongoose.Schema({
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name: "Welcome to your todolist!"
+  name: "Welcome to your todolist!",
 });
- 
+
 const item2 = new Item({
-  name: "Hit the + button to add a new item."
+  name: "Hit the + button to add a new item.",
 });
- 
+
 const item3 = new Item({
-  name: "<-- Hit this to delete an item."
+  name: "<-- Hit this to delete an item.",
 });
- 
+
 const defaultItems = [item1, item2, item3];
 
 const listSchema = {
   name: String,
-  items: [itemsSchema]
+  items: [itemsSchema],
 };
 
-const List = mongoose.model("List",listSchema);
+const List = mongoose.model("List", listSchema);
 
-app.get("/", function(req, res) {
-
+app.get("/", function (req, res) {
   Item.find({})
-  .then(foundItems => {
-
-    if (foundItems.length === 0) {
-      Item.insertMany(defaultItems)
-        .then(() => {
-          console.log('Items inserted successfully!');
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+    .then((foundItems) => {
+      if (foundItems.length === 0) {
+        Item.insertMany(defaultItems)
+          .then(() => {
+            console.log("Items inserted successfully!");
+          })
+          .catch((error) => {
+            console.error(error);
+          });
         res.redirect("/");
-    } else {
-      res.render("list", {listTitle: "Today", newListItems: foundItems});
-    }
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error);
-  });
-
+      } else {
+        res.render("list", { listTitle: "Today", newListItems: foundItems });
+      }
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error);
+    });
 });
 
-app.get("/:customListName", function(req,res){
-  const customListName = req.params.customListName;
+app.get("/:customListName", function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName })
     .then((foundList) => {
       if (foundList) {
-        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
       } else {
         const list = new List({
           name: customListName,
-          items: defaultItems
+          items: defaultItems,
         });
-      
+
         list.save();
         res.redirect("/" + customListName);
       }
     })
     .catch((error) => {
-      console.error('Error occurred while finding document:', error);
+      console.error("Error occurred while finding document:", error);
     });
-
 });
 
-app.post("/", function(req, res){
-
+app.post("/", function (req, res) {
   const itemName = req.body.newItem;
   const listName = req.body.list;
 
   const item = new Item({
-    name: itemName
+    name: itemName,
   });
 
-  if(listName === "Today"){
+  if (listName === "Today") {
     item.save();
     res.redirect("/");
-  } else{
-
+  } else {
     List.findOne({ name: listName })
-    .then((foundList) => {
-      
-      if (foundList !== null) {
-        if (foundList.items) {
-          foundList.items.push(item);
-        } else {
-          foundList.items = [item];
+      .then((foundList) => {
+        if (foundList !== null) {
+          if (foundList.items) {
+            foundList.items.push(item);
+          } else {
+            foundList.items = [item];
+          }
+
+          foundList.save();
         }
-      }
-        foundList.save();
+
         res.redirect("/" + listName);
-      
-    })
-    .catch((error) => {
-      console.error('Error occurred while finding document:', error);
-    });
-
-    }
-
+      })
+      .catch((error) => {
+        console.error("Error occurred while finding document:", error);
+      });
+  }
 });
 
-app.post("/delete", function(req,res){
+app.post("/delete", function (req, res) {
   const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
 
-  Item.findByIdAndRemove(checkedItemId)
-  .then((removedDocument) => {
-    if (removedDocument) {
-      console.log('Document removed successfully:', removedDocument);
-      res.redirect("/");
-    } else {
-      console.log('No document found with the provided ID:', documentId);
-    }
-  })
-  .catch((error) => {
-    console.error('Error occurred while removing document:', error);
-  });
-
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId)
+      .then((removedDocument) => {
+        if (removedDocument) {
+          console.log("Document removed successfully:", removedDocument);
+          res.redirect("/");
+        } else {
+          console.log("No document found with the provided ID:", documentId);
+        }
+      })
+      .catch((error) => {
+        console.error("Error occurred while removing document:", error);
+      });
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } }
+    )
+      .then((foundList) => {
+        res.redirect("/" + listName);
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+      });
+  }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000,()=>{
-    console.log("Server started on port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
